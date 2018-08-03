02ecd125bbca1775e8ddb65eb45e08a40a6cea29
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-03 01:38:23 -0500

    Add files via upload

--- server/express.js
+++ server/express.js
@@ -8,15 +8,17 @@ import Template from './../template'
 
 const app = express()
 
+app.get('/', (req, res) => {
+  res.status(200).send(Template())
+})
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
-app.get('/', (req, res) => {
-  res.status(200).send(Template())
-})
+
+
 
 export default app
-
