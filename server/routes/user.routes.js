df244b23e50e74db8ab6767024e8853946c61ef6
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-05 11:11:09 -0500

    Update user.routes.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -6,3 +6,13 @@ const router = express.Router()
 router.route('/api/users')
 	.get(userCtrl.list)
 	.post(userCtrl.create)
+	
+
+router.route('./api/users/:userId')
+	.get(userCtrl.read)
+	.put(userCtrl.update)
+	.delete(userCtrl.remove)
+	
+router.parm('userId', userCtrl.userById)
+
+export default router
