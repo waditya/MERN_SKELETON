71fe7299419811bade24010463e34e78f996a41b
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-05 11:08:27 -0500

    Update user.routes.js

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,1 +1,8 @@
-baghana
+import express from 'express'
+import userCtrl from '../controllers/user.controller'
+
+const router = express.Router()
+
+router.route('/api/users')
+	.get(userCtrl.list)
+	.post(userCtrl.create)
