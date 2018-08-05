42b77cae3a9d105fb593a4c8386ef0d5afe71dc9
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-05 10:51:47 -0500

    Update dbErrorHandler.js

--- server/helpers/dbErrorHandler.js
+++ server/helpers/dbErrorHandler.js
@@ -1,1 +1,20 @@
-Wahe Guru ki Fateh, Wahe guru ka khalsa
+const getErrorMessage = (err) => {
+	let message = ''
+	if (err.code){
+		switch (err.code){
+			case 11000:
+			case 11001:
+				message = getUniqueErrorMessage(err)
+				break
+			default:
+				message = 'Something went wrong'
+		}
+	}else{
+		for (let errName in err.errors){
+			if(err.errors[errName].message)
+		}
+	}
+	return message
+}
+
+export default {getErrorMessage}
