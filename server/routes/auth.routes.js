5511a08bbfcd866b2eace4e8943a8a7cf8a4ecab
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-05 16:25:22 -0500

    Update auth.routes.js

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,1 +1,11 @@
-baddi badi batein aur wada pav khate
+import express from 'express'
+import authCtrl from '../controllers/auth.controller'
+
+const router = express.Router()
+
+router.route('/auth/signin')
+  .post(authCtrl.signin)
+router.route('/auth/signout')
+  .get(authCtrl.signout)
+
+export default router
