38fcc653711af7db427525d2411d7b2cc8dc7318
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-05 10:59:11 -0500

    Update dbErrorHandler.js

--- server/helpers/dbErrorHandler.js
+++ server/helpers/dbErrorHandler.js
@@ -28,4 +28,6 @@ const getUniqueErrorMessage = (err) => {
 		err.message.lastIndexOf('_1'))
 		output = fieldName.charAt(0).toUpperCase()+ fieldName.slice(1) +
 		'already exists'
+	}catch(ex){
+		output = 'Unique field already exists'
 	}
