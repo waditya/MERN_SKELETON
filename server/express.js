5746f3ca394cac80efd7447c029b83859edea72c
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-06 01:06:43 -0500

    Update express.js

--- server/express.js
+++ server/express.js
@@ -5,6 +5,8 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 import Template from './../template'
+import userRoutes from './routes/user.routes'
+import authRoutes from './routes/auth.routes'
 
 const app = express()
 
@@ -18,6 +20,14 @@ app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
+app.use('/', userRoutes)
+app.use('/', authRoutes)
+
+app.use((err, req, res, next) => {
+  if (err.name === 'UnauthorizedError') {
+    res.status(401).json({"error" : err.name + ": " + err.message})
+  }
+})
 
 
 
