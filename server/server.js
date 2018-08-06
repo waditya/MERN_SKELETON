1cad7fd3ad37e3537862b1082a06e011361a62ba
Aditya Dhananjay Wagholikar <waditya02@gmail.com>
2018-08-06 01:07:26 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`)
 })
 
+
 app.listen(config.port, (err) => {
   if (err) {
     console.log(err)
